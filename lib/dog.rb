406be1f2946a667ce9b8e04c9b1c58564da5e69c
KeslieBox <kesliebox@gmail.com>
2021-01-19 20:14:42 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def name=(name = nil)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed = nil)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name=(name=nil)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job=nil)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+end
